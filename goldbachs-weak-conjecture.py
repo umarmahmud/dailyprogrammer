ad3a8dd118ee57b32bf612428983cfc171c2eb6a
umarmahmud <umar.mahmud@outlook.com>
2018-04-11 15:46:43 -0400

    list of tuples

--- goldbachs-weak-conjecture.py
+++ goldbachs-weak-conjecture.py
@@ -1,3 +1,5 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/8bh8dh/20180411_challenge_356_intermediate_goldbachs/
+
 def goldbach_weak_conjecture(n):
     
     def find_primes(n):
@@ -10,6 +12,7 @@ def goldbach_weak_conjecture(n):
                     is_prime = False
             if is_prime:
                 l.append(i)
+                
         return l
     
     list_of_primes = find_primes(n)
@@ -19,9 +22,7 @@ def goldbach_weak_conjecture(n):
         for j in range(i, len(list_of_primes)):
             for k in range(j, len(list_of_primes)):
                 if list_of_primes[i] + list_of_primes[j] + list_of_primes[k] == n:
-                    sum_list.append(list_of_primes[i])
-                    sum_list.append(list_of_primes[j])
-                    sum_list.append(list_of_primes[k])
-                    sum_list.append("-------")
+                    sum_list.append((list_of_primes[i], list_of_primes[j], list_of_primes[k]))
                     
     return sum_list
+
