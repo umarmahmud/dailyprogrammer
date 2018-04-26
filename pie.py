b39f6abb9060bcf70f59d0d6233696158c1f60fd
umarmahmud <umar.mahmud@outlook.com>
2018-04-26 19:24:50 -0400

    added nested function for code reuse

--- pie.py
+++ pie.py
@@ -3,15 +3,17 @@ from collections import OrderedDict
 
 def number_of_pies(ingredients):
     pumpkin_pie_recipie = {'pumpkin-flavoring': 1,
-                   'eggs': 3,
-                   'milk': 4,
-                   'sugar': 3
-                  }
-    apple_pie_recipie = {'apples': 1,
-                 'eggs': 4,
-                 'milk': 3,
-                 'sugar': 2
-                }
+                           'apples': 0,
+                           'eggs': 3,
+                           'milk': 4,
+                           'sugar': 3
+                          }
+    apple_pie_recipie = {'pumpkin-flavoring': 0,
+                         'apples': 1,
+                         'eggs': 4,
+                         'milk': 3,
+                         'sugar': 2
+                        }
     available_ingredients = {'pumpkin-flavoring': 0,
                             'apples': 0,
                             'eggs': 0,
@@ -22,44 +24,39 @@ def number_of_pies(ingredients):
     # add ingredients array (the input) to dictionary values
     available_ingredients = dict(zip(available_ingredients, ingredients))
     
-    # count number of pumpkin pies we can make
+    # get max number of pumpkin/apple pies we can make
+    def get_max_number(ingredients, recipie):
+        pies = tuple(zip(ingredients.values(), recipie.values()))
+        quotient = []
+
+        for i in range(len(pies)):
+            for j in range(1):
+                if pies[i][j+1] == 0:
+                    continue
+                quotient.append(int(pies[i][j]/pies[i][j+1]))
+
+        return min(quotient)
     
-    # copy as not to modify original dictionary
-    pumpkin_pies = available_ingredients.copy()
+    max_no_of_pumpkin_pies = get_max_number(available_ingredients, pumpkin_pie_recipie)
+    max_no_of_apple_pies = get_max_number(available_ingredients, apple_pie_recipie)
     
-    pumpkin_pies.pop('apples')
-    
-    pumpkin_pies = tuple(zip(pumpkin_pies.values(), pumpkin_pie_recipie.values()))
-    
-    print(pumpkin_pies)
-    
-    pumpkin_quotient = []
-    
-    for i in range(len(pumpkin_pies)):
-        for j in range(1):
-            pumpkin_quotient.append(int(pumpkin_pies[i][j]/pumpkin_pies[i][j+1]))
-    
-    print(min(pumpkin_quotient))
-                  
-    # count number of apple pies we can make
-    
-    #copy as not to modify original dictionary
-    apple_pies = available_ingredients.copy()
-    
-    apple_pies.pop('pumpkin-flavoring')
-    
-    apple_pies = tuple(zip(apple_pies.values(), apple_pie_recipie.values()))
-    
-    print(apple_pies)
-    
-    apple_quotient = []
-    
-    for i in range(len(apple_pies)):
-        for j in range(1):
-            apple_quotient.append(int(apple_pies[i][j]/apple_pies[i][j+1]))
+    if max_no_of_pumpkin_pies > max_no_of_apple_pies:
+        majority = 'pumpkin_pies'
+        remainder = 'apple_pies'
+    elif max_no_of_apple_pies > max_no_of_pumpkin_pies:
+        majority = 'apple_pies'
+        remainder = 'pumpkin_pies'
+    else:
+        # if max numbers are tied
+        pass
+        
+    # determine number of ingredients we will need
+    if majority == 'pumpkin_pies':
+        for k, v in pumpkin_pie_recipie.items():
+            v *= max_no_of_pumpkin_pies
+            print(k, v)
             
-    print(min(apple_quotient))
-    
-    # take both counts of pies, take larger value. After that, subtract ingredients used for larger value pie and see max number
-    # of other kind of pie that can be made with remaining ingredients
+        print(remainder)
+        
+    # subtract values, determine max number of other kind of pie that can be made with remaining ingredients
     
