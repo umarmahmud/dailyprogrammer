2a8e4e467c053feface40326c210c467d42d9e7d
umarmahmud <umar.mahmud@outlook.com>
2018-04-25 23:48:03 -0400

    initial commit

---
+++ pie.py
@@ -0,0 +1,18 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/87rz8c/20180328_challenge_355_intermediate_possible/
+
+def number_of_pies(ingredients):
+    pumpkin_pie = {1: 'pumpkin-flavoring',
+                   3: 'eggs',
+                   4: 'milk',
+                   3: 'sugar'
+                  }
+    apple_pie = {1: 'apple',
+                 4: 'eggs',
+                 3: 'milk',
+                 2: 'sugar'
+                }
+    
+    
+    
+    
+    
